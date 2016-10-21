605c3125681f441577ebd63ff985975f7ddae0f9
Jean-Sébastien Beaulieu <folkrav@gmail.com>
2016-10-20 20:35:50 -0400

    added db migration tools

--- manage.py
+++ manage.py
@@ -3,14 +3,16 @@ import os
 from app import create_app, db
 from app.models import User, PrivilegeGroup, Article, Tag, ArticleCategory, ArticleStatus
 from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User)
 manager.add_command('shell', Shell(make_context=make_shell_context))
-# manager.add_command('db', MigrateCommand)  # to add when migration is written
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
