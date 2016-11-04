c0e131cb955930cdac0ae597f97449a1d5578094
Jean-Sébastien Beaulieu <folkrav@gmail.com>
2016-11-04 01:30:54 -0400

    fixed pagination

--- app/articles/views.py
+++ app/articles/views.py
@@ -5,6 +5,7 @@ from app import POSTS_PER_PAGE
 
 
 @articles.route('/text')
+@articles.route('/text/<int:page>')
 def text(page=1):
     posts = Article.query.filter_by(status=ArticleStatus.query.get(ArticleStatus.PUBLISHED)).order_by(Article.timestamp.desc()).paginate(page, POSTS_PER_PAGE, True)
     return render_template('articles/text.html', posts=posts)
